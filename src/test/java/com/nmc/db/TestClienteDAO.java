148bc702e008cbc0f8896e5d840494eca3fd3309
Isaac <37666684+IsaacMCorpas@users.noreply.github.com>
2018-07-18 17:05:18 +0200

    subida de nuevo

---
+++ src/test/java/com/nmc/db/TestClienteDAO.java
@@ -0,0 +1,23 @@
+package com.nmc.db;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ncm.db.ClienteDAO;
+
+public class TestClienteDAO extends TestNCM {
+	private static Logger logger = Logger.getLogger("TestClienteDAO");
+
+	@BeforeClass
+	public static void setUpClass() {
+		setUpContext();
+	}
+	
+	
+	
+	
+}
